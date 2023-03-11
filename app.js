dab622d3e9fe9aa9d65b16fca1e1da5b0f8f640a
Mubarakbose <109033079+Mubarakbose@users.noreply.github.com>
2023-03-11 16:14:59 +0100

    comment

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ let init = true;
 
   }
 
-  // The three possible autocomes of players' choice in the game
+  // The three possible autocomes of players' choice
   function computerScissors(playerSelection, computerSelection, computerScore, playerScore) {
 
     if (computerSelection?.toLowerCase() === "scissors") {
